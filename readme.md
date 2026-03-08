362c3ce93a0e9192e7ffaae048c6030be64da23a
Jace Callihoo <jacecallihoo@gmail.com>
2026-03-08 12:29:33 -0600

    -

--- readme.md
+++ readme.md
@@ -1,41 +1,25 @@
-# audio editor tool
+# Wave File Generator
 
-- should create .wav files
-- should edit .wav files? (maybe later)
-- should be able to write to files
-- how do I choose the endianness of a written byte
+## what it does
+Generate audio data in the wave file format based on combined audio waves
 
+## how it does it
+Audio generation is handled through a streaming approach, where each buffer of samples is computed on demand and written to the output file or audio device before the next chunk is processed. This ensures low memory overhead and scalable audio production.
 
+implemented multiple wave types
+sin, square, sawtooth, triangle
+
+imlemented linear transformation 
+used runtime polymorphism to allow for both constant and linear transformations to aplitude and frequency
+
+implemented linear transformatinos for frequency by calculating the average area under the curve for the given keyframes
+
+accounted for memory endianness
+
+## wave functions explained
+each function accepts a time and frequency and returns the amlpitude of the wave at that time. 
+The time is always relative to 0 as this prevents phase cancelation when adding waves together
 
 ### References
 https://web.archive.org/web/20250610165025/http://soundfile.sapp.org/doc/WaveFormat/
-
-
-```
-// ChunkID          constant
-// ChunkSize        derived
-// Format           constant
-// Subchunk1ID      constant
-// Subchunk1Size    constant maybe?
-// AudioFormat      constant (1 for PCM)
-// NumChannels      variable (1 = mono, 2 = stereo)
-// SampleRate       variable (8000, 44100, etc)
-// ByteRate         derived
-// BlockAlign       derived
-// BitsPerSample    variable (8 = 8 bits, 16 = 16 bits)
-// ExtraParmasSize  other (if PCM, then doesn't exist)
-// ExtraParams      other (space for extra parameters)
-// Subchunk2ID      constant ("data")
-// Subchunk2Size    derived (NumSamples * NumChannels * BitsPerSample/8)
-// Data             variable
-```
-
-
-## Wave Math
-
-Time (duration) x sample rate is used to determine the number of samples
-For each sample, sample index x sample rate is used to determine time
-Time x frequency used to determine amplitude
-
-
 
--- src/main.cpp
+++ src/main.cpp
@@ -1,55 +1,20 @@
-#include <arpa/inet.h>
 #include <iostream>
 #include <fstream>
 #include <vector>
-#include <cstdint>
-#include <array>
-#include <span>
-#include <cmath>
-#include <numbers>
-#include <cassert>
-#include <algorithm>
-#include <format>
-#include <algorithm>
-#include <memory>
 
 #include "wave.hpp"
 #include "json.hpp"
 
-#define assertm(exp, msg) assert((void(msg), exp))
-
 #define BUFFER_SIZE 500
 
-// TODO: shoulde the function names be camel case? Check google formatting standards
-// TODO: check the class field names as well
-
-// TODO: make a generator version of this function
-// TODO: make a stateless version of this function that gets a saple at a given index
-
-// TODO: maybe make this later, or find a way to modify the square wave maybe? Maybe have a translation
-// template <typename T>
-// std::vector<T> pulseWave(int durationMs, int sampleRate, int bitsPerSample, int frequency, T amplitude) {
-// }
-
-// TODO: should test this more, it could have bugs
-// should this function cache it's previous state for performance? too early to tell
-// should this function just return the multiplier?
-
-
 int main() {
-    
-    // set input values
     const int numChannels = 1;
     const int sampleRate = 44100;
     const int bitsPerSample = 16;
     const int bytesPerSample = bitsPerSample / 8;
 
-    // create sound with input values
-    // std::vector<std::uint8_t> data = createSound(numChannels, sampleRate, bitsPerSample);
-    // create wav file with input values
     const int durationMs = 2000;
     const double frequency = 440.0;
-    // const std::int16_t amplitude = 16383;
     const size_t numSamples = (durationMs * sampleRate) / 1000;
     const size_t delaySamples = 0;
 
@@ -69,8 +34,6 @@ int main() {
         { .value = 0.0, .percent = 1.0 },
     };
 
-    // const double frequency = 440.0;
-
     std::vector<Keyframe> s1FrequencyKeyframes = {
         { .value = double(frequency), .percent = 0.0 },
         { .value = double(frequency*2), .percent = 1.0 },
@@ -81,25 +44,9 @@ int main() {
         { .value = double(frequency*4), .percent = 1.0 },
     };
 
-    // for (double percent = 0.0; percent <= 1.0; percent += 0.01) {
-    //     X x = keyframeAverage(amplitudeUpKeyframes, percent);
-    //     std::cout << std::format("percent: {}\npreviousAverage: {}\ncurrent: {}\n\n", percent, x.previousAverage, x.current);
-    // }
-    // exit(0);
-
-    // Sound s1(sinWave, delaySamples, numSamples, sampleRate, std::make_unique<Constant>(440), keyframes);
-    // Sound s1 = {
-    //     .function = sinWave,
-    //     .delaySamples = 0,
-    //     .numSamples = numSamples,
-    //     .sampleRate = sampleRate,
-    //     .frequency = std::make_unique<Constant>(frequency),
-    //     .linearTransformKeyframes = keyframes,
-    // };
-
     Sound s1 = {
         .function = sinWave,
-        .delaySamples = 0,
+        .delaySamples = delaySamples,
         .numSamples = numSamples,
         .sampleRate = sampleRate,
         .frequency = std::make_unique<Linear>(s1FrequencyKeyframes),
@@ -108,26 +55,12 @@ int main() {
 
     Sound s2 = {
         .function = sinWave,
-        .delaySamples = 0,
+        .delaySamples = delaySamples,
         .numSamples = numSamples,
         .sampleRate = sampleRate,
         .frequency = std::make_unique<Linear>(s2FrequencyKeyframes),
         .amplitude = std::make_unique<Linear>(keyframes),
     };
-
-
-    // TEST //////////////
-    // const size_t i = 699;
-    // const double percent = double(i) / numSamples;
-    // X x = keyframeAverage(amplitudeUpKeyframes, percent);
-    // std::cout << std::format("outside x: current = {}, previousAverage = {}\n", x.current, x.previousAverage);
-    // const double normalAmplitude2 = squareWave(i, sampleRate, x.previousAverage);
-    // const double normalAmplitude = squareWave2(i, sampleRate, amplitudeUpKeyframes, double(i)/ numSamples);
-    // std::cout << std::format("diff: {}\na: {}\nb: {}\npercent: {}\n\n", i, normalAmplitude2, normalAmplitude, double(i)/numSamples);
-    // exit(0);
-
-    ///////////////
-
 
     std::ofstream file("output.wav", std::ios::binary);
     if (!file) {
@@ -136,65 +69,23 @@ int main() {
     }
 
     std::array header = createHeader(numChannels, sampleRate, bitsPerSample, numSamples * bytesPerSample);
-    // const int UP = 1;
-    // const int DOWN = 2;
-
-    // int state = UP;
-    // size_t previousPeriodStart = 0;
     file.write(reinterpret_cast<const char*>(header.data()), header.size());
 
-    // numSamples * (bitsPerSample / 8)
     for (size_t i = 0; i < numSamples;) {
         std::array<std::uint8_t, BUFFER_SIZE> bytes;
+
         for (size_t j = 0; j < BUFFER_SIZE / bytesPerSample; j++, i++) {
             const double normalAmplitude = combine({&s1, &s2}, i);
-
-            // const double x = combine({&s1}, i);
-            // const double x = combine({&s2}, i);
-
-            // if (x > 0.0 && state == DOWN) {
-            //     state = UP;
-            //     size_t numSamplesInPeriod = i - previousPeriodStart;
-            //     double periodDuration = double(numSamplesInPeriod) / sampleRate;
-            //     double frequency = 1 / periodDuration;
-            //     std::cout << std::format("wave frequency: {}\nperiodDuration: {}s\nnumSamplesInPeriod: {}\n\n", frequency, periodDuration, numSamplesInPeriod);
-            //     previousPeriodStart = i;
-            // } else if (x < 0.0 && state == UP) {
-            //     state = DOWN;
-            //     // do nothing else here since it's only half
-            // }
-
-            // std::string buffer(20, ' ');
-            // size_t index = std::min((1 + normalAmplitude) * 10, double(19));
-            // buffer[index] = '.';
-            // std::cout << buffer << '\n';
-
             const auto quantizedAmplitude = quantize<std::int16_t>(normalAmplitude, bitsPerSample);
             const auto byteAmplitude = toLittleEndian(quantizedAmplitude);
+
             writeAtOffset(byteAmplitude, j * bytesPerSample, bytes);
-            // bytes.insert(bytes.end(), byteAmplitude.begin(), byteAmplitude.end());
         }
+
         file.write(reinterpret_cast<const char*>(bytes.data()), bytes.size());
     }
 
-    // for (size_t i = 0; i < bytes.size(); i++) {
-    //     std::cout << std::hex << int(bytes[i]) << " ";
-    // }
-    // std::cout << std::endl;
-
-    // Wav wave(numChannels, sampleRate, bitsPerSample, bytes);
-
-    // std::vector<std::uint8_t> buffer = wave.toBytes();
-
-
-    //////////////////////////////////////////////////////////////////////
-    // write to file
-
     file.close();
-
-
-    // file.write(reinterpret_cast<const char*>(buffer.data()), buffer.size());
-    // optional, this is called when the file leaves scope
 
     return 0;
 }
